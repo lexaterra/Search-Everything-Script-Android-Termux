8f4b6cbb6a086e7c378661dfff6bb1bf0ef63cea
lex81 <68161611+lex81@users.noreply.github.com>
2023-06-03 12:40:40 -0400

    Add files via upload

---
+++ searchall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Author: Alex Terranova 2023
+#TITLE: Search All
+# AT Widget
+response=''
+INT='\033[0;31m'
+SDCARD='\033[0;32m'
+REG='\033[0;0m'
+printf "\033c"
+
+while ! [[ $response = "q" ]]
+do
+
+echo ''
+read -p 'Find What? ' response
+echo ''
+
+echo -e "${INT}INTERNAL${REG}"
+cd '/storage/emulated/0/'
+find . -iname *$response* 2>/dev/null
+
+#replace 668B-CC32 with the name of your sdcard
+echo -e "${SDCARD}EXTERNAL${REG}"
+cd '/storage/668B-CC32/'
+find . -iname *$response* 2>/dev/null
+
+done
